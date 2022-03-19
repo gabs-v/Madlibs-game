80d1d6639276d3bbfa4513f168d0b907b15b1d12
gabs-v <gabyvazquez8297@gmail.com>
2022-03-19 16:41:49 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+#A simple Madlips game to get familiat wioth user input
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color + ",")
+print(plural_noun + " are blue,")
+print("I love " + celebrity + ".")
